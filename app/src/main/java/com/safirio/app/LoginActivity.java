bf71077d22c16d5a8dd4715e2b1a7c9e99f57dbc
Noel Hwande <noel.hwande@gmail.com>
2014-02-07 14:37:57 -0600

    Initial commit

---
+++ app/src/main/java/com/safirio/app/LoginActivity.java
@@ -0,0 +1,38 @@
+package com.safirio.app;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
+
+/**
+ * Created by noelhwande on 2/7/14.
+ */
+public class LoginActivity extends Activity {
+
+    protected TextView mSignUpTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mSignUpTextView = (TextView)findViewById(R.id.signUpText);
+        mSignUpTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.login, menu);
+        return true;
+    }
+}
---
+++ app/src/main/java/com/safirio/app/SignUpActivity.java
@@ -0,0 +1,25 @@
+package com.safirio.app;
+
+/**
+ * Created by noelhwande on 2/7/14.
+ */
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class SignUpActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.sign_up, menu);
+        return true;
+    }
+
+}
