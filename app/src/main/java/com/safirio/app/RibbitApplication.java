d8f390521c78a5393cb9c1c83872dbb0b6600df5
Noel Hwande <noel.hwande@gmail.com>
2014-02-09 19:10:18 -0600

    Added Parse library

---
+++ app/src/main/java/com/safirio/app/RibbitApplication.java
@@ -0,0 +1,23 @@
+package com.safirio.app;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+/**
+ * Created by noelh on 2/9/14.
+ */
+public class RibbitApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Parse.initialize(this, "crip4hmJ1EipdkXIAcZKfJo0tRjXq5zteh563sFo", "7YoYXvECojOCuk1F6kTGFzpl3pyKxpgJsq7hTIa0");
+
+        ParseObject testObject = new ParseObject("TestObject");
+        testObject.put("foo", "bar");
+        testObject.saveInBackground();
+    }
+
+}
